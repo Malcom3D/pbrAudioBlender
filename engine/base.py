330b337adaaacde5885b3410eb0971cd2da4e715
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-07 18:19:38 +0200

    refactor(pbrAudioRender): review dask config for blender addon

--- engine/base.py
+++ engine/base.py
@@ -68,6 +68,7 @@ class PBRAudioRenderEngine(RenderEngine):
 #        progress_step = 0.5 / len(scene.pbraudio.collision_collection.objects.values())
 #        update_progress(progress_step)
         scene = depsgraph.scene
+        decimals = 18
         export = render_exporter.RenderExporter(scene=scene, decimals=decimals)
         start_frame = scene.frame_start
         end_frame = scene.frame_end

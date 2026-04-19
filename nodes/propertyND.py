3433db34b68f001d9f1eb321be893289f4178ad4
Malcom3D <Malcom3D@users.noreply.github.com>
2026-04-19 15:40:59 +0200

    feat(pbrAudioRender): pbrAudioRay design development

--- nodes/propertyND.py
+++ nodes/propertyND.py
@@ -41,7 +41,7 @@ class AcousticPropertiesNode(AcousticMaterialNode):
         name="absorption",
         description="WideBand absorption coeff",
         default=0.5,
-        update=sync_data
+        update=sync_data,
         min=0.0,
         max=1.0
     )

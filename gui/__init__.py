d4b22415a2ada847913489533f18b04352ff868b
Malcom3D <Malcom3D@users.noreply.github.com>
2026-06-17 18:50:43 +0200

    feat(pbrAudioBleder): AudioForces denoising and signal smoothing postprocess panels

--- gui/__init__.py
+++ gui/__init__.py
@@ -32,6 +32,7 @@ def register():
     bpy.utils.unregister_class(bpy.types.DATA_PT_empty)
 
     for cls in classes:
+        print(cls.bl_idname)
         register_class(cls)
 
     # Add menu to 3D Viewport Add menu
